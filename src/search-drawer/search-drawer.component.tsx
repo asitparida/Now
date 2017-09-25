daa874c75d0468c2107bbfa35198f3074a232f7b
Asit Parida <asitparida@live.in>
2017-09-25 12:48:21 +0530

    Fix search drawer search invoke

--- src/search-drawer/search-drawer.component.tsx
+++ src/search-drawer/search-drawer.component.tsx
@@ -19,13 +19,31 @@ interface OwnProps {
     backgroundColor: string;
 }
 
-interface OwnState { }
+interface OwnState { 
+    searchToken: string;
+}
 
 class SearchDrawer extends React.Component<OwnProps, OwnState> {
     domRef: any;
     escape: boolean = true;
+    searchToken = '';
     constructor(props: any) {
         super(props);
+        this.state = {
+            searchToken: ''
+        };
+        this.updateToken = this.updateToken.bind(this);
+        this.launchSearch = this.launchSearch.bind(this);
+    }
+    launchSearch($event: any) {
+        if ($event.keyCode === 13 && this.state.searchToken !== '') {
+            window.location.assign('https://www.google.co.in/search?q=' + this.state.searchToken);
+        }
+    }
+    updateToken(e: any) {
+        this.setState({
+            searchToken: (e.target.value as string).trim()
+        });
     }
     // tslint:disable:max-line-length
     render() {
@@ -36,7 +54,7 @@ class SearchDrawer extends React.Component<OwnProps, OwnState> {
                         <video src="https://player.vimeo.com/external/226624946.hd.mp4?s=6857972b29b70e164494b409185fc6b5b63ba7ad&profile_id=175" autoPlay={true} loop={true} />
                         <div className="search-container">
                             <div className="input-holder">
-                                <input type="text" placeholder="Search" />
+                                <input type="text" value={this.state.searchToken} placeholder="Search" onChange={this.updateToken} onKeyUp={this.launchSearch} />
                                 <IconButton className="search-mic">
                                     <AvMic color="rgba(255, 255, 255, 0.560)" />
                                 </IconButton>
